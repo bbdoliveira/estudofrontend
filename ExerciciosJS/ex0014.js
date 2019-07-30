c504f39c47621e2d47c479f2c3649bae292884e0
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2019-07-30 19:49:07 -0300

    For, Do While e While Funcionando Juntos

--- ExerciciosJS/ex0014.js
+++ ExerciciosJS/ex0014.js
@@ -1,14 +1,17 @@
-var a = 1
-var b = 1
-console.log('Começou!')
-do {
-    c = 1
-    while (c <= 3) {
-        console.log('Inicio teste (While)!')
+var a
+for (a = 1; a <= 2; a++){ //FOR tem Inicio, Teste Lógico e Incremento.
+    var b = 1
+    console.log('|---------------FOR----------------|')
+    console.log('Começou!')
+do { // Do While - Executa o bloco e testa no final.
+    var c = 1
+    console.log('Inicio teste (While)!')
+    while (c < 6) { //While -  Teste no inicio, depois executa o bloco.
         console.log(c)
         c++
     }
     console.log('Fim teste (While)!')
-} while (b <=2) {
     b++
+} while (b <=2)
+console.log('|------------Fim do FOR------------|')
 }
