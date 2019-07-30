a6d31c5ad4d5ddc9aed69afeef311406e1000b6c
Bruno Bonifácio de Oliveira <bruno.oliveira@granol.com.br>
2019-07-30 17:33:30 -0300

    Testes while e do while

---
+++ ExerciciosJS/ex0014.js
@@ -0,0 +1,14 @@
+var a = 1
+var b = 1
+console.log('Começou!')
+do {
+    c = 1
+    while (c <= 3) {
+        console.log('Inicio teste (While)!')
+        console.log(c)
+        c++
+    }
+    console.log('Fim teste (While)!')
+} while (b <=2) {
+    b++
+}
