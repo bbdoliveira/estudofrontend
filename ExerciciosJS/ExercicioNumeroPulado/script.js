1137e9f0dbb9f9f4c433d87a742551d726af544a
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2019-07-31 23:32:18 -0300

    Logica principal implementada!

--- ExerciciosJS/ExercicioNumeroPulado/script.js
+++ ExerciciosJS/ExercicioNumeroPulado/script.js
@@ -1,25 +1,26 @@
 function contar(params) {
-    /*var inicio = document.getElementById('txtinicio')
+    var inicio = document.getElementById('txtinicio')
     var final = document.getElementById('txtfim')
     var passo = document.getElementById('txtpasso')
     var ini = Number(inicio.value)
     var fin = Number(final.value)
-    var pas = Number(passo.value)*/
+    var pas = Number(passo.value)
     var res = document.querySelector('div#res')
-    var ini = 5
-    var fin = 10
-
-    while (ini <= fin) {
-        res.innerHTML = (`Teste ${ini}`)
-        ini++
-    }
-
-    /*if (pas == 0) { //Caso passo seja 0 ele recebe 1
+    //res.innerHTML = (`Teste ${ini}, ${fin} e ${pas}`)
+    if (ini <= 0) {
+        window.alert('Inicio zerado ou vazio. Impossivel Contar!')
+    } else if (fin <= 0) {
+        window.alert('Final zerado ou vazio. Impossivel contar!')
+    } else if (pas <= 0) { //Caso passo seja 0 ele recebe 1
         window.alert('[ERRO] Passo Inválido! Considerando passo 1!')
         pas = 1
-        window.alert(`Teste passo ${pas}`)
-    }*/
-
-    //window.alert(`Teste ${ini}, ${fin} e ${pas}`)
-
+        //window.alert(`Teste passo ${pas}`)
+    } else if (ini <= 0 && fin <= 0 && pas <= 0) {
+        window.alert('[ERRO] Formulário vazio! Tente novamente!')
+    } else {
+        while (ini <= fin) {
+            res.innerHTML = (`${ini}`)
+            ini = ini + pas
+        }
+    }
 }
---
+++ ExerciciosJS/ExercicioNumeroPulado/teste.js
@@ -0,0 +1,8 @@
+var ini = 1
+var fin = 10
+var pas = 2
+
+while (ini <= fin) {
+    console.log(ini)
+    ini = ini + pas
+}
