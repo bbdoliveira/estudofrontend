c27bbd4eb76c0d1dd054ab397bc305eae49fa24a
Bruno Bonifácio de Oliveira <bruno.oliveira@granol.com.br>
2019-08-07 17:49:52 -0300

    Atualização Exercicio

--- ExerciciosJS/ExercicioNumeroPulado/script.js
+++ ExerciciosJS/ExercicioNumeroPulado/script.js
@@ -2,26 +2,25 @@ function contar(params) {
     var inicio = document.getElementById('txtinicio')
     var final = document.getElementById('txtfim')
     var passo = document.getElementById('txtpasso')
-    var ini = Number(inicio.value)
-    var fin = Number(final.value)
-    var pas = Number(passo.value)
     var res = document.querySelector('div#res')
-    var count
-    //res.innerHTML = (`Teste ${ini}, ${fin} e ${pas}`)
-    /*if (ini.value.length == 0 || fin.value.length == 0 || pas.value.length == 0) {
-        window.alert('[ERRO] Faltam Dados! Tente novamente!')
-    } else {
-        while (ini <= fin) {
-            res.innerHTML = (`${ini}`)
-            ini = ini + pas
-        }
-    }*/
+    
     if (inicio.value.length == 0 || final.value.length == 0 || passo.value.length == 0) {
         alert ('[ERRO] Faltam Dados!')
     } else {
         res.innerHTML = 'Contando: '
-        for (count = ini; count <= fin; count += pas){
-            res.innerHTML += ` - ${count}`
+        var ini = Number(inicio.value)
+        var fin = Number(final.value)
+        var pas = Number(passo.value)
+        //var count
+        if (ini < fin) {
+            for (let count = ini; count <= fin; count += pas){
+                res.innerHTML += `${count} \u{1F449} `
+            }
+        } else {
+            for (let count = ini; count >= fin; count -= pas){
+                res.innerHTML = `${count} \u{1F448}`
+            }
         }
     }
+    res.innerHTML = `\u{1F3C1}`
 }
