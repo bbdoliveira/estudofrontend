6610102a494dcd7a19ee7bf0dd0cfdc72da0b2fe
Bruno Bonifácio de Oliveira <bruno.oliveira@granol.com.br>
2019-08-16 18:31:00 -0300

    Criação arquivo JS

---
+++ script.js
@@ -0,0 +1,21 @@
+//Cadastro
+function validarEmail(params) {
+    alert ('Teste')
+}
+function validarSenha(params) {
+    alert ('Teste')
+}
+function logar(params) {
+    var senha = document.getElementById('txtSenha')
+    var email = document.getElementById('txtEmail')
+    let teste = "teste"
+    if (teste != senha) {
+        alert ('Sua senha esta errada!')
+    } else {
+        alert ('Seja bem vindo!')
+    }
+    /*alert (`Vc digitou ${senha}`)*/
+}
+function cadastrar(params) {
+    alert ('Teste')
+}
