21e338fc55ab211873449d08487cce7c9330e82e
Bruno Bonifácio de Oliveira <bruno.oliveira@granol.com.br>
2019-11-18 19:11:23 -0300

    Começo Script tabuada

---
+++ ExerciciosJS/ExercicioTabuada/script.js
@@ -0,0 +1,10 @@
+function tabuada (params) {
+    let num = document.getElementById('txtnum')
+    let tab = document.getElementById('seltab')
+    
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um numero!')
+    } else {
+        let n = Number(num.value)
+    }
+}
