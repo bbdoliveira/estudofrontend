847925524c35e4d217e5659937048db3b7c7edaf
Bruno Bonifácio de Oliveira <bruno.oliveira@granol.com.br>
2019-11-19 17:20:26 -0300

    Exercicio Array

---
+++ ExerciciosJS/ExercicioArray/array.js
@@ -0,0 +1,17 @@
+let num = [5, 8, 6, 9, 3, 7, 1, 2]
+let pos
+/*
+console.log(`O meu vetor tem ${num.length} posições!`)
+console.log(`A posição 3 do meu vetor é: ${num[2]}`)
+console.log(`O primeiro numero do meu vetor é: ${num[0]}`)
+console.log(`A ordem crescente do meu vetor é: ${num.sort()}`)
+*/
+/* Forma padrão de escrever o array na tela.
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(`A posição atual é: ${num[pos]}`)
+    
+}*/
+
+for (let pos in num) {
+    console.log(`Aposição ${pos} tem o numero: ${num[pos]}`)
+}
