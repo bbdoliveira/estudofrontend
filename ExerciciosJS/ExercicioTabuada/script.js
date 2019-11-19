128eef75307948a1f96ffcf7fc088e5e627a4f67
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2019-11-19 00:41:14 -0300

    Conclusão Ex. Tabuada

--- ExerciciosJS/ExercicioTabuada/script.js
+++ ExerciciosJS/ExercicioTabuada/script.js
@@ -3,8 +3,18 @@ function tabuada (params) {
     let tab = document.getElementById('seltab')
     
     if (num.value.length == 0) {
+        tab.innerHTML = ''
         window.alert('Por favor, digite um numero!')
     } else {
         let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')//Cria options no select.
+            item.text = `${n} x ${c} = ${n*c}` //Altera o texto dos options
+            item.value = `tab${c}` //Adiciona um value nos options.
+            tab.appendChild(item)
+            c++
+        }
     }
 }
