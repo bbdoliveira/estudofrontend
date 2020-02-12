687df3a09acd70a35ff14e9012b8dd2846404b04
Bruno Bonifácio de Oliveira <bruno.oliveira@granol.com.br>
2020-02-12 17:21:22 -0300

    Novo Exemplo

---
+++ ExerciciosJS/TestesJS/script.js
@@ -0,0 +1,14 @@
+/*function negrito() {
+    //let marcado = document.querySelector('#italico')
+    let texto = document.querySelector("#texto")
+    alert(texto)
+}*/
+function teste() {
+    //let texto = 'Teste'
+    let texto = document.querySelector('input#texto')
+    //let texto = document.getElementById('texto')
+    alert(texto)
+    //console.log(texto)
+}
+//let texto = 'Teste'
+//console.log(texto)
